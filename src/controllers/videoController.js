7c48926a2ce0ef27f484d7226e03312815c11f32
kangdongil <akang8150@naver.com>
2021-10-18 11:43:06 +0000

    4.8 URL Parameters

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,9 @@
 import express from "express";
 
 export const recommended = (req, res) => res.send("Home");
-export const watch = (req, res) => res.send("Watch Video");
+export const watch = (req, res) => {
+	return res.send(`Watch Video: #${req.params.id}`);
+}
 export const edit = (req, res) => res.send("Edit Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
 export const search = (req, res) => res.send("Search Video");
